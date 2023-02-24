6faabdc140385022ecc54101e90f2e0e0f4a9e4f
Nika <99128017+Tesla0802@users.noreply.github.com>
2023-02-24 18:47:25 +0400

    new class js

--- main.js
+++ main.js
@@ -8,63 +8,7 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 
 const gravity = 0.7
 
-class Sprite{
-    constructor({position, velocity, color = '#ff0000', offset}) {
-        this.position = position
-        this.velocity = velocity
-        this.width = 50
-        this.height = 150
-        this.lastKey
-        this.attackBox = {
-          position: {
-            x: this.position.x,
-            y: this.position.y
-          },
-          offset,
-          width: 100,
-          height: 50,
-        }
-        this.color = color
-        this.isAttacking
-        this.health = 100
-    }
-    draw(){
-        c.fillStyle = this.color
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-
-        //attack box
-        if(this.isAttacking) {
-        c.fillStyle = '#008000'
-        c.fillRect(
-        this.attackBox.position.x, 
-        this.attackBox.position.y,
-        this.attackBox.width,
-        this.attackBox.height)
-        }
-    }
-
-    update() {
-        this.draw()
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-        this.attackBox.position.y = this.position.y
-
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-
-        if(this.position.y + this.height + this.velocity.y >= canvas.height){
-            this.velocity.y = 0
-        }else this.velocity.y += gravity
-    }
-
-    attack() {
-      this.isAttacking = true
-      setTimeout(() => {
-        this.isAttacking = false
-      }, 100)
-    }
-}
-
-const player = new Sprite({
+const player = new Fighter({
     position: {
     x: 0,
     y: 0
@@ -79,9 +23,7 @@ velocity: {
   }
 })
 
-
-
-const enemy = new Sprite({
+const enemy = new Fighter({
     position: {
     x: 400,
     y: 100
@@ -96,8 +38,6 @@ velocity: {
     y: 0
   }
 })
-
-
 
 console.log(player)
 
